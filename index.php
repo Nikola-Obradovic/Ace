abfd26455f7dfd32191f6348eb8429cb2770a5e7
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-04-24 19:46:12 +0200

    filter buttons added (not functional)

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Discover</title>
 
+
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/media.css">
 
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
@@ -52,6 +55,21 @@
 
 </header>
 
+<main class="margin-top-lg">
+<section class="red-circles container flex flex-gap-lg wrapped">
+    <div class="flex flex-column flex-gap-tiny"><button class="red-circle-button"><i class="fa-solid fa-car"></i></button><p>Cars</p>
+    </div>
+    <div class="flex flex-column flex-gap-tiny"><button class="red-circle-button"><i class="fa-solid fa-house"></i></button><p>Housings</p></div>
+    <div class="flex flex-column flex-gap-tiny"><button class="red-circle-button"><i class="fa-solid fa-mobile-screen-button"></i></button><p>Smartphones</p></div>
+    <div class="flex flex-column flex-gap-tiny"><button class="red-circle-button"><i class="fa-solid fa-shoe-prints"></i></button><p>Shoes</p></div>
+    <div class="flex flex-column flex-gap-tiny"><button class="red-circle-button"><i class="fa-solid fa-laptop"></i></button><p>Laptops</p></div>
+
+</section>
+
+
+
+
+</main>
 
 <script src="main.js"></script>
 </body>
