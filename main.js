44970be60df017ab17da4428fae89a5f0231f0b5
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-04-24 03:40:02 +0200

    new pages + link styling added

--- main.js
+++ main.js
@@ -2,7 +2,28 @@ const header = document.querySelector(".header");
 
 const btnToggleNav = document.querySelector(".btn-mobile-nav");
 
+//hamburger event listener
 btnToggleNav.addEventListener("click", () =>
     header.classList.toggle("nav-open")
 );
 
+
+
+
+const navLinks = document.querySelectorAll('.main-nav-link');
+const pageURL = window.location.pathname
+
+/* runs each time the page is reloaded (when you open page for the first time or press link to another page), not an event listener because it would not apply the property when you open the page for the first time*/
+for (const link of navLinks) {
+
+    if (pageURL.includes(link.getAttribute('href').split('/').pop())){
+
+        link.classList.add('current-link');
+
+    }else if (link.classList.contains('current-link')){
+
+        link.classList.remove('current-link')
+    }
+
+
+}
--- post-new-listing.php
+++ post-new-listing.php
@@ -5,8 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Discover</title>
-
+    <title>Post New Listing</title>
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/media.css">
@@ -15,12 +14,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <script
-            type="module"
-            src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"
+        type="module"
+        src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"
     ></script>
     <script
-            nomodule=""
-            src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.js"
+        nomodule=""
+        src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.js"
     ></script>
 </head>
 <body>
