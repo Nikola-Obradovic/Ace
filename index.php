039cf7bd94fd641a95decda0e1cabc563b91f736
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-04-28 03:51:55 +0200

    bug fix

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/media.css">
 
+    <!-- LATO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script
@@ -184,7 +188,7 @@
                         <input type="text" class="general-text--input">
                     </div>
                     <div class="margin-bottom-md">
-                        <p class="margin-bottom-sm">Storeage:</p>
+                        <p class="margin-bottom-sm">Storage:</p>
                         <input type="text" class="general-text--input">
                     </div>
                     <div class="margin-bottom-md">
--- post-new-listing.php
+++ post-new-listing.php
@@ -10,9 +10,11 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/media.css">
 
+    <!-- LATO -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
     <script
         type="module"
         src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"
